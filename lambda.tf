e4f0951bc7ea64894cc172907390114eb1c2d1de
aaron lim <aaron.limse@hotmail.com>
2025-04-16 21:58:57 +0800

    test

--- lambda.tf
+++ lambda.tf
@@ -9,8 +9,9 @@ data "archive_file" "lambda" {
 # Defines the lambda function to be created using file
 
 resource "aws_lambda_function" "own_lambda" {
-  function_name    = var.lambda_function_name
-  handler          = "lambda_function.lambda_handler"
+  function_name = var.lambda_function_name
+  # handler          = "lambda_function.lambda_handler"
+  handler          = "lambda_function.handler"
   runtime          = "nodejs22.x"
   role             = aws_iam_role.iam_for_lambda.arn
   filename         = data.archive_file.lambda.output_path         # Adjust the path
